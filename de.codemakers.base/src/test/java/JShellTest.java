ba22cbfa3ee80de982cb0e953923e602a58079f9
Paul Hagedorn <panzer1119@gmail.com>
2018-04-08 22:27:23 +0200

    Improved the JShell Test and added auto saving result maekaeniks
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- de.codemakers.base/src/test/java/JShellTest.java
+++ de.codemakers.base/src/test/java/JShellTest.java
@@ -21,9 +21,12 @@ import de.codemakers.base.util.JavaScriptEngineBuilder;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.concurrent.TimeUnit;
 
 public class JShellTest {
+
+    public static boolean AUTO_SAVE_RESULTS = true;
+    public static boolean AUTO_ADD_RETURN = true;
+    public static int lastResult = 0;
 
     public static final void main(String[] args) {
         final JavaScriptEngineBuilder javaScriptEngineBuilder = new JavaScriptEngineBuilder();
@@ -34,8 +37,12 @@ public class JShellTest {
                 final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
                 while (true) {
                     String line = bufferedReader.readLine();
+                    if (line.trim().isEmpty()) {
+                        continue;
+                    }
                     if (line.startsWith("/")) {
                         line = line.substring(1);
+                        boolean recognized_command = true;
                         if (line.equalsIgnoreCase("exit") || line.equalsIgnoreCase("stop") || line.equalsIgnoreCase("shutdown") || line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("q")) {
                             System.out.println("Stopped the " + JavaScriptEngine.class.getSimpleName());
                             CJP.shutdown().complete();
@@ -43,14 +50,36 @@ public class JShellTest {
                             javaScriptEngine.reset();
                             System.out.println("Resetted the " + JavaScriptEngine.class.getSimpleName());
                         } else if (line.equalsIgnoreCase("help") || line.equalsIgnoreCase("h")) {
-                            System.out.println("Usage:\n/exit\n/help\n/reset\n/put VARIABLE DATA\n/get VARIABLE");
-                        } else if (line.startsWith("put ")) {
+                            System.out.println(String.format("Usage:\n" +
+                                    "/exit - Exits the %s\n" +
+                                    "/get VARIABLE - Returns the DATA for the VARIABLE\n" +
+                                    "/help - Shows this help\n" +
+                                    "/reset - Resets the %s\n" +
+                                    "/set VARIABLE DATA - Sets the DATA to the VARIABLE\n" +
+                                    "/set VARIABLE /eval CODE - Sets the evaluated CODE to the VARIABLE\n" +
+                                    "/toggle auto_add_return - Toggles if a missing \"return\" will be added before each CODE\n" +
+                                    "/toggle auto_save_results - Toggles if a result from an evaluation should be saved in a VARIABLE\n", JShellTest.class.getSimpleName(), JavaScriptEngine.class.getSimpleName()));
+                        } else if (line.startsWith("toggle ")) {
+                            line = line.substring("toggle ".length());
+                            if (line.equalsIgnoreCase("auto_save_results") || line.equalsIgnoreCase("asr")) {
+                                AUTO_SAVE_RESULTS = !AUTO_SAVE_RESULTS;
+                                System.out.println("Toggled AUTO_SAVE_RESULTS to " + AUTO_SAVE_RESULTS);
+                            } else if (line.equalsIgnoreCase("auto_add_return") || line.equalsIgnoreCase("aar")) {
+                                AUTO_ADD_RETURN = !AUTO_ADD_RETURN;
+                                System.out.println("Toggled AUTO_ADD_RETURN to " + AUTO_ADD_RETURN);
+                            } else {
+                                recognized_command = false;
+                            }
+                        } else if (line.startsWith("put ") || line.startsWith("set ")) {
                             line = line.substring("put ".length());
                             final String variable = line.substring(0, line.indexOf(" "));
-                            String data = line.substring(variable.length() + " ".length());
-                            if (data.startsWith("/eval ")) {
-                                data = data.substring("/eval ".length());
-                                data = javaScriptEngine.execute(data);
+                            Object data = line.substring(variable.length() + " ".length());
+                            if (data.toString().startsWith("/eval return ")) {
+                                data = data.toString().substring("/eval ".length());
+                                if (AUTO_ADD_RETURN && !data.toString().startsWith("return ")) {
+                                    data = "return " + data.toString();
+                                }
+                                data = javaScriptEngine.execute(data.toString());
                             }
                             javaScriptEngine.put(variable, data);
                             System.out.println(String.format("Setted \"%s\" to \"%s\"", variable, data));
@@ -58,16 +87,32 @@ public class JShellTest {
                             line = line.substring("get ".length());
                             System.out.println(String.format("\"%s\" is \"%s\"", line, javaScriptEngine.get(line)));
                         } else {
-                            System.err.println(String.format("Unrecognized command: %s", line));
+                            recognized_command = false;
+                        }
+                        if (!recognized_command) {
+                            System.err.println(String.format("Unrecognized command: %s (Use /help for help)", line));
                         }
                     } else {
-                        javaScriptEngine.executeLarge(line).queue(System.out::println, System.err::println);
+                        if (AUTO_ADD_RETURN && !line.startsWith("return ")) {
+                            line = "return " + line;
+                        }
+                        javaScriptEngine.executeLarge(line).queue((result) -> {
+                            if (AUTO_SAVE_RESULTS) {
+                                lastResult++;
+                                final String variable = String.format("$%d", lastResult);
+                                javaScriptEngine.put(variable, result);
+                                System.out.println(String.format("%s => %s", variable, result));
+                            } else {
+                                System.out.println("=> " + result);
+                            }
+                        }, System.err::println);
                     }
                 }
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-        }); thread.start();
+        });
+        thread.start();
         //CJP.shutdown().queueAfter(5, TimeUnit.MINUTES);
     }
 
